ffcbefc9d3e48e69c592d20324ae0c7d7fab1b64
Britdan (Aidan) <71894636+greatbritdan@users.noreply.github.com>
2024-02-16 17:40:32 +0000

    Added navigation between folders

--- class/windows/fileManager.lua
+++ class/windows/fileManager.lua
@@ -2,8 +2,26 @@ WindowFileManager = Class("fileManager", Window)
 
 function WindowFileManager:initialize(desktop, x, y, w, h)
     Window.initialize(self, desktop, x, y, w, h, "file manager")
-    self.elements.path = UI.input({x=24, y=4, w=self.w-28, h=16, text="b:/", mc=50, resize=function (element)
-        element.w = self.w-28
+    self.elements.path = UI.input({x=24, y=4, w=self.w-48, h=16, text="b:/", mc=50, resize=function (element)
+        element.w = self.w-48
+    end})
+    self.elements.back = UI.button({x=self.w-20, y=4, w=16, h=16, text="(", func=function (element)
+        local path = self.elements.path.text
+        if path == "b:/" then
+            return
+        end
+        path = string.gsub(path, "^b:/", "")
+        path = Split(path, "/")
+        table.remove(path, #path)
+        print(path)
+        if #path > 0 then
+            path = table.concat(path, "/").."/"
+            self.elements.path.text = "b:/"..path
+        else
+            self.elements.path.text = "b:/"
+        end
+    end, resize=function (element)
+        element.x = self.x+self.w-20
     end})
     self:sync()
 end
@@ -20,18 +38,48 @@ function WindowFileManager:draw()
     -- Print out all files in path
     local files = self.desktop:getFile(self.elements.path.text)
     if files then
-        love.graphics.setColor({1,1,1})
         local y = self.y+self.navbar.h+28
-        for _, file in ipairs(files) do
-            if file.type ~= "folder" then
-                love.graphics.print(file.name.."."..file.type, self.x+24, y)
-            else
-                love.graphics.print(file.name, self.x+24, y)
+        for i, file in ipairs(files) do
+            love.graphics.setColor({0.5,0.5,0.5})
+            if self:hoveringFile() == i then
+                love.graphics.setColor({1,1,1})
             end
-            y = y + 12
+            if file.type ~= "folder" then
+                love.graphics.print(file.name.."."..file.type, self.x+44, y+4)
+            else
+                love.graphics.print(file.name, self.x+44, y+4)
+            end
+            love.graphics.draw(IconsImg, IconsQuads[file.type], self.x+24, y)
+            y = y + 20
         end
     end
 
     -- Draw UI
     Window.drawUI(self)
+end
+
+function WindowFileManager:mousepressed(mx, my, b)
+    Window.mousepressed(self, mx, my, b)
+    local hover = self:hoveringFile()
+    if hover then
+        local files = self.desktop:getFile(self.elements.path.text)
+        if files[hover].type == "folder" then
+            self.elements.path.text = self.elements.path.text..files[hover].name.."/"
+        end
+    end
+end
+
+function WindowFileManager:hoveringFile()
+    local mx, my = love.mouse.getX()/Env.scale, love.mouse.getY()/Env.scale
+    local files = self.desktop:getFile(self.elements.path.text)
+    if files then
+        local y = self.y+self.navbar.h+28
+        for i, file in ipairs(files) do
+            if AABB(mx, my, 1, 1, self.x+24, y, self.w-28, 20) then
+                return i
+            end
+            y = y + 20
+        end
+    end
+    return false
 end
